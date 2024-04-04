f12ef919e2773159d1ab9453d79e409f7d107cd8
BlasterWhite <icarambala@gmail.com>
2024-04-04 13:09:35 +0200

    Fix errors

--- src/pages/Section/SectionView.jsx
+++ src/pages/Section/SectionView.jsx
@@ -2,21 +2,15 @@ import PropTypes from 'prop-types';
 import './SectionView.scss';
 import { MultipleChoiceComponent } from '@/composants/MultipleChoiceComponent.jsx';
 
-export function SectionView({section, handleSectionClicked}) {
-    const {texte, sections} = section;
+export function SectionView({ section, handleSectionClicked }) {
+  const { texte, sections, image } = section;
 
-
-
-  let image;
-  fetch('http://193.168.146.103:3000/images/2')
-    .then((response) => response.json())
-    .then((data) => (image = data));
   return (
     <div className={'section-view'}>
       <div className={'scenario'}>
         <p className={'text-scenario'}>{texte}</p>
         <img
-          src={image != null ? image : '/src/assets/images/placeholder.png'}
+          src={image?.image ? image.image : '/src/assets/images/placeholder.png'}
           alt={'Section image'}
         />
       </div>
@@ -32,7 +26,8 @@ SectionView.propTypes = {
   section: PropTypes.shape({
     id: PropTypes.number.isRequired,
     texte: PropTypes.string.isRequired,
-    sections: PropTypes.array.isRequired
+    sections: PropTypes.array.isRequired,
+    image: PropTypes.object
   }).isRequired,
   handleSectionClicked: PropTypes.func.isRequired
 };
