1f5e0e1e33b71bd56edfdbf4bca76c93ba464537
BlasterWhite <icarambala@gmail.com>
2024-04-04 15:09:50 +0200

    FIX bad implementation

--- src/composants/DiceComponent/DiceComponent.jsx
+++ src/composants/DiceComponent/DiceComponent.jsx
@@ -1,12 +1,20 @@
 import './DiceComponent.scss';
 import { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export function DiceComponent({ numberOfDices = 2, numberOfFaces = 6, sendResultsToParent }) {
+export function DiceComponent({
+  numberOfDices = 2,
+  numberOfFaces = 6,
+  handleNextSection,
+  section
+}) {
   const isMounted = useRef(false);
   const [hasDiceResults, setHasDiceResults] = useState(false);
   const [results, setResults] = useState([]);
 
   const dices = useRef([]);
+
+  console.log('section', section);
 
   if (!isMounted.current) {
     for (let i = 0; i < numberOfDices; i++) {
@@ -23,6 +31,25 @@ export function DiceComponent({ numberOfDices = 2, numberOfFaces = 6, sendResult
     isMounted.current = true;
   }, []);
 
+  function handleResults(results) {
+    const win = section.resultat.condition?.['1'];
+    const winDestination = section.resultat.gagne;
+    const loseDestination = section.resultat.perd;
+
+    const resultSum = results.reduce((acc, curr) => acc + curr, 0);
+    console.log('result', results);
+    console.log('resultSum', resultSum);
+    console.log('win', win);
+    console.log('winDestination', winDestination);
+    console.log('loseDestination', loseDestination);
+
+    if (win && win.length > 0 && win.includes(resultSum)) {
+      handleNextSection(winDestination);
+    } else {
+      handleNextSection(loseDestination);
+    }
+  }
+
   function throwDices() {
     setResults([]);
     let immediateResults = [];
@@ -31,7 +58,7 @@ export function DiceComponent({ numberOfDices = 2, numberOfFaces = 6, sendResult
         dices.current[i][Math.floor(Math.random() * dices.current[i].length)] + 1;
       setResults((results) => [...results, immediateResults[i]]);
     }
-    sendResultsToParent(immediateResults);
+    handleResults(immediateResults);
     if (!hasDiceResults) {
       setHasDiceResults(true);
     }
@@ -53,3 +80,21 @@ export function DiceComponent({ numberOfDices = 2, numberOfFaces = 6, sendResult
     </div>
   );
 }
+
+DiceComponent.propTypes = {
+  section: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    texte: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    sections: PropTypes.array.isRequired,
+    image: PropTypes.object,
+    resultat: PropTypes.shape({
+      condition: PropTypes.object,
+      gagne: PropTypes.number,
+      perd: PropTypes.number
+    }).isRequired
+  }).isRequired,
+  handleNextSection: PropTypes.func,
+  numberOfDices: PropTypes.number,
+  numberOfFaces: PropTypes.number
+};
