2bb0206b562e4df3cc422e30a22ff553f9e3514f
BlasterWhite <icarambala@gmail.com>
2024-04-04 14:39:48 +0200

    Fix

--- src/pages/Section/SectionView.jsx
+++ src/pages/Section/SectionView.jsx
@@ -3,7 +3,24 @@ import './SectionView.scss';
 import { MultipleChoiceComponent } from '@/composants/MultipleChoiceComponent.jsx';
 
 export function SectionView({ section, handleSectionClicked }) {
-  const { texte, sections, image } = section;
+  const { texte, sections, image, type } = section;
+
+  function interracivity() {
+    if (type) {
+      if (type === 'choix')
+        return (
+          <MultipleChoiceComponent
+            sections={sections}
+            handleSectionClicked={(e) => handleSectionClicked(e)}
+          />
+        );
+      if (type === 'combat') return <h2>Combat</h2>;
+      if (type === 'enigme') return <h2>Enigme</h2>;
+      if (type === 'des') return <h2>Des</h2>;
+      if (type === 'termine') return <h2>Fin</h2>;
+      if (type === 'none') return <h2>None</h2>;
+    }
+  }
 
   return (
     <div className={'section-view'}>
@@ -14,10 +31,7 @@ export function SectionView({ section, handleSectionClicked }) {
           alt={'Section image'}
         />
       </div>
-      <MultipleChoiceComponent
-        sections={sections}
-        handleSectionClicked={(e) => handleSectionClicked(e)}
-      />
+      {interracivity()}
     </div>
   );
 }
