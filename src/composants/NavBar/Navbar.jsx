b357d0a721ceeedf1f354200fccd7b7c450179f5
maximefurious <maxime.boujeant@gmail.com>
2024-04-04 10:10:12 +0200

    feat/ add login, register, logout and context for futur usage

--- src/composants/NavBar/Navbar.jsx
+++ src/composants/NavBar/Navbar.jsx
@@ -1,17 +1,39 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
+import { AuthContext } from '../AuthContext/AuthContext.jsx';
+import { useContext } from 'react';
 
 export function Navbar() {
+  const { user, logout, isLoggedIn } = useContext(AuthContext);
+
   return (
     <nav className={'nav-header'}>
       <div className={'nav-item-left'}>
         <NavLink to={'/'}>Home</NavLink>
         <NavLink to={'/book'}>Library</NavLink>
       </div>
-      <div className={'nav-item-right'}>
-        <NavLink to={'/login'}>Login</NavLink>
-        <NavLink to={'/register'}>Register</NavLink>
-      </div>
+      {!isLoggedIn ? (
+        <div className={'nav-item-right'}>
+          <NavLink to={'/login'}>Login</NavLink>
+          <NavLink to={'/register'}>Register</NavLink>
+        </div>
+      ) : (
+        <div className={'nav-item-right'}>
+          <span>
+            {user && user.firstname && user.lastname ? (
+              <div className={'user-info'}>
+                <span className={'user-name'}>
+                  {user.firstname} {user.lastname}
+                </span>
+                <span className={'user-email'}>{user.email}</span>
+              </div>
+            ) : (
+              'Unknown user'
+            )}
+          </span>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
     </nav>
   );
 }
--- src/pages/Account/LoginView.jsx
+++ src/pages/Account/LoginView.jsx
@@ -1,8 +1,82 @@
+import './Login.scss';
+import { AuthContext } from '../../composants/AuthContext/AuthContext';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export function LoginView() {
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = {
+      email: email,
+      password: password
+    };
+
+    try {
+      await fetch('http://localhost:3000/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      }).then((response) =>
+        response.json().then((data) => {
+          if (!data.error) {
+            login(data);
+            navigate('/');
+          } else {
+            const errorDiv = document.querySelector('.form-error');
+            errorDiv.textContent = 'Invalid email or password';
+          }
+        })
+      );
+    } catch (error) {
+      console.error('Erreur lors de la soumission du formulaire de connexion:', error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      TODO: Implement login form
-    </div>
+    <form className="login-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="email" className="form-label">
+          Email :
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-input"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password" className="form-label">
+          Password :
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-input"
+          required
+        />
+      </div>
+      <div className="form-error"></div>
+      <div className="form-last-row">
+        <span className="login-link">
+          <a href="/register">You don&apos;t have an account yet ?</a>
+        </span>
+        <button type="submit" className="submit-button">
+          Login
+        </button>
+      </div>
+    </form>
   );
 }
