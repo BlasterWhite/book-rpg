47ff54815e6978e2db91dd8d99d25d1b2fd38e5d
BlasterWhite <icarambala@gmail.com>
2024-04-05 11:56:26 +0200

    Start

--- src/pages/Book/SingleBook.jsx
+++ src/pages/Book/SingleBook.jsx
@@ -1,20 +1,22 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { SectionView } from '@/pages/Section/SectionView.jsx';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '@/composants/AuthContext/AuthContext.jsx';
 
 export function SingleBook() {
   const { sectionId, bookId, characterId } = useParams();
   const navigate = useNavigate();
 
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImVtYWlsIjoidGVzdC50ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTcxMjIyMjA5MCwiZXhwIjoxNzEyMzA4NDkwfQ.Pde893oQq8kHp7BsGYAkD5Vfl07iDyDGUENp2u7vRyE';
   const apiURL = import.meta.env.VITE_API_URL || 'http://193.168.146.103:3000';
+
+  const { user } = useContext(AuthContext);
 
   const [section, SetSection] = useState({});
 
   useEffect(() => {
+    if (!user) return;
     fetch(`${apiURL}/livres/${bookId}/sections/${sectionId}`, {
-      headers: { Authorization: token }
+      headers: { Authorization: user.token }
     }).then((response) => {
       if (response.ok) {
         response.json().then((data) => {
@@ -24,7 +26,7 @@ export function SingleBook() {
         console.error('Error fetching sections');
       }
     });
-  }, [bookId, sectionId, apiURL]);
+  }, [bookId, sectionId, apiURL, user]);
 
   function renderSection() {
     if (section && section.id) {
