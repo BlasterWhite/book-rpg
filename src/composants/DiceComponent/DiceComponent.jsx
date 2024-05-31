a00f96ddfd7d37a478dd3ce1b104ba44920101d1
maximefurious <maxime.boujeant@gmail.com>
2024-05-31 23:03:44 +0200

    feat/ add event trigger useEffect

--- src/composants/DiceComponent/DiceComponent.jsx
+++ src/composants/DiceComponent/DiceComponent.jsx
@@ -4,22 +4,25 @@ import PropTypes from 'prop-types';
 import { useAuth } from '@/contexts/AuthContext.jsx';
 
 export function DiceComponent({
-  numberOfDices = 2,
-  numberOfFaces = 6,
-  handleNextSection,
-  section,
-  characterId
-}) {
+                                currentSection,
+                                numberOfDices = 2,
+                                numberOfFaces = 6,
+                                handleNextSection,
+                                section,
+                                characterId
+                              }) {
   const isMounted = useRef(false);
   const [hasDiceResults, setHasDiceResults] = useState(false);
   const [results, setResults] = useState([]);
   const [feedback, setFeedback] = useState('');
   const [aventure, setAventure] = useState([]);
   const { user } = useAuth();
+  const eventIsDispatched = useRef();
+
+  const API_URL = import.meta.env.VITE_API_URL || 'http://193.168.146.103:3000';
 
   useEffect(() => {
     if (!user) return;
-    const API_URL = import.meta.env.VITE_API_URL || 'http://193.168.146.103:3000';
     const requestOptions = {
       method: 'GET',
       headers: { 'Content-Type': 'application/json', Authorization: user.token }
@@ -30,7 +33,6 @@ export function DiceComponent({
         .then((data) => setAventure(data))
         .catch((error) => console.error(error))
     );
-    // on fait une requête put sur l'aventure
   }, [characterId, section, user]);
 
   const dices = useRef([]);
@@ -49,6 +51,24 @@ export function DiceComponent({
   useEffect(() => {
     isMounted.current = true;
   }, []);
+
+  useEffect(() => {
+    if (eventIsDispatched.current) return;
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json', Authorization: user.token },
+      body: JSON.stringify({ events: currentSection.events })
+    };
+
+    fetch(`${API_URL}/personnages/${characterId}/events`, requestOptions).then((response) =>
+      response
+        .json()
+        .catch((error) => console.error(error))
+    );
+
+    currentSection.events;
+    eventIsDispatched.current = true;
+  }, [API_URL, characterId, currentSection.events, user.token]);
 
   function handleResults(results) {
     const win = section.resultat.condition?.['1'];
@@ -73,7 +93,6 @@ export function DiceComponent({
       handleNextSection(finalDestination);
 
       if (!user) return;
-      const API_URL = import.meta.env.VITE_API_URL || 'http://193.168.146.103:3000';
       // on récupère la prochaine sections depuis sections
       if (!section) return;
       const statut = section.type === 'termine' ? 'termine' : 'en_cours';
@@ -86,7 +105,6 @@ export function DiceComponent({
       fetch(`${API_URL}/aventures/${newAventureID}`, requestOptions).then((response) =>
         response
           .json()
-          .then((data) => console.log(data))
           .catch((error) => console.error(error))
       );
     }, 2000);
@@ -125,6 +143,7 @@ export function DiceComponent({
 }
 
 DiceComponent.propTypes = {
+  currentSection: PropTypes.object.isRequired,
   section: PropTypes.shape({
     id: PropTypes.number.isRequired,
     texte: PropTypes.string.isRequired,
