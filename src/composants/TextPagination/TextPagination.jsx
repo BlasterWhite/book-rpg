c7b3ce99b1ec9aa5008edb4a0bd86078d16238b0
BlasterWhite <icarambala@gmail.com>
2024-06-04 21:45:16 +0200

    Added logic pagination + style

--- src/composants/TextPagination/TextPagination.jsx
+++ src/composants/TextPagination/TextPagination.jsx
@@ -2,39 +2,69 @@ import './TextPagination.scss';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-const maxChar = 200;
+const maxChar = 700;
 
 export function TextPagination({ text }) {
-  const [numberOfPages, setNumberOfPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   const [textsArray, setTextsArray] = useState([]);
 
+  function splitText(text) {
+    const words = text.split(' ');
+    const pages = [];
+    let currentPage = '';
+    words.forEach((word) => {
+      if (currentPage.length + word.length < maxChar) {
+        currentPage += word + ' ';
+      } else {
+        pages.push(currentPage);
+        currentPage = word + ' ';
+      }
+    });
+    pages.push(currentPage);
+    return pages;
+  }
+
   useEffect(() => {
-    setNumberOfPages(Math.ceil(text.length / maxChar));
-    setTextsArray(text.match(new RegExp(`.{1,${maxChar}}`, 'g')));
-    setCurrentPage(1);
+    setTextsArray(splitText(text));
+    setNumberOfPages(splitText(text).length);
+    setCurrentPage(0);
   }, [text]);
 
   function handleNextPage() {
-    setCurrentPage(currentPage + 1);
+    if (currentPage <= numberOfPages) {
+      setCurrentPage(currentPage + 1);
+    }
   }
 
   function handlePreviousPage() {
-    setCurrentPage(currentPage - 1);
+    if (currentPage >= 1) {
+      setCurrentPage(currentPage - 1);
+    }
   }
 
   return (
     <div className={'text-pagination'}>
       <p>{textsArray[currentPage]}</p>
-      <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-        Previous
-      </button>
-      <span>
-        {currentPage}/{numberOfPages}
-      </span>
-      <button onClick={handleNextPage} disabled={currentPage === numberOfPages}>
-        Next
-      </button>
+      {numberOfPages > 1 ? (
+        <div className={'actions'}>
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage === 0}
+            className={'previous-page'}>
+            <img src={'/icons/pagination_left.svg'} alt={'previous'} />
+          </button>
+          <span>
+            {currentPage + 1} / {numberOfPages}
+          </span>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === numberOfPages - 1}
+            className={'next-page'}>
+            <img src={'/icons/pagination_right.svg'} alt={'next'} />
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
