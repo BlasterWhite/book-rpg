4286da4d0aca119e59b0dd4d9c41f55ac3457359
stgourmelon <steven.gourmelon@etudiant.univ-rennes1.fr>
2024-06-05 17:41:24 +0200

    Manque style popup; Manque appel API pour  update et delete; sinon ok

--- src/pages/Account/ProfilView.jsx
+++ src/pages/Account/ProfilView.jsx
@@ -14,8 +14,16 @@ export function ProfilView() {
     creation_date: '',
     livre: {},
   });
+  const [userAdventureData, setUserAdventureData] = useState();
   const [favoris, setFavoris] = useState([{}]);
   const [books, setBooks] = useState([]);
+  const [clickOnUpdate, setClickOnUpdate] = useState(false);
+  const [clickOnDelete, setClickOnDelete] = useState(false);
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const apiURL = import.meta.env.VITE_API_URL || 'http://193.168.146.103:3000';
 
@@ -26,7 +34,23 @@ export function ProfilView() {
       headers: { 'Content-Type': 'application/json', Authorization: user.token }
     };
     fetch(`${apiURL}/users/${user.id}`, requestOptions)
-      .then((response) => response.json().then((data) => setUserData(data)))
+      .then((response) => response.json().then((data) => {
+        setUserData(data);
+        setFirstname(data.prenom);
+        setLastname(data.nom);
+        setEmail(data.mail);
+      }))
+      .catch((error) => console.error(error));
+  }, [apiURL, user]);
+
+  useEffect(() => {
+    if (!user) return;
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', Authorization: user.token }
+    };
+    fetch(`${apiURL}/users/aventures`, requestOptions)
+      .then((response) => response.json().then((data) => setUserAdventureData(data)))
       .catch((error) => console.error(error));
   }, [apiURL, user]);
 
@@ -80,8 +104,97 @@ export function ProfilView() {
     return dt.toLocaleString(DateTime.DATE_MED);
   };
 
+  const updateProfile = () => {
+    setClickOnUpdate(true);
+  }
+
+  const saveProfile = () => {
+    setClickOnUpdate(false);
+  }
+
+  const deleteProfile = () => {
+    setClickOnDelete(true);
+  }
+
+  const cancelDeleteProfile = () => {
+    setClickOnDelete(false);
+  }
+
+  const changeFirstname = (firstname) => {
+    setFirstname(firstname);
+  };
+
+  const changeLastname = (firstname) => {
+    setLastname(firstname);
+  };
+
+  const changeEmail = (email) => {
+    setEmail(email);
+  };
+
+  const changePassword = (password) => {
+    setPassword(password);
+  };
+
+  const changeConfirmPassword = (confirmPassword) => {
+    setConfirmPassword(confirmPassword);
+  };
+
+  const totalAdventure = () => {
+    if(!userAdventureData) return 0;
+    return userAdventureData.length;
+  }
+
+  const adventurePercentageCompleted = () => {
+    //console.log(userAdventureData);
+    if(!userAdventureData) return 0;
+    let percentage = 0;
+    userAdventureData.map((adventure) => {
+      if(adventure.statut === 'termine') percentage++;
+    });
+    return Math.floor((percentage/userAdventureData.length)*100);
+  };
+  
+  const booksFinished = () => {
+    if(!userAdventureData) return 0;
+    let finishedBooks = [];
+    userAdventureData.map((adventure) => {
+      if(adventure.statut === 'termine' && !finishedBooks.includes(adventure.id_livre)) finishedBooks.push(adventure.id_livre);
+    });
+    return finishedBooks.length;
+  }
+
+  const mostFavoriteBooks = () => {
+    if(!userAdventureData || !books) return;
+    let mostFavoriteBooksId = [];
+    let mostFavoriteBooksTitle = [];
+    userAdventureData.map((adventure) => {
+      if(mostFavoriteBooksId[adventure.id_livre] === undefined) mostFavoriteBooksId[adventure.id_livre] = 1
+      else mostFavoriteBooksId[adventure.id_livre]++
+    });
+
+    const maxValue = Math.max(...Object.values(mostFavoriteBooksId));
+    const maxKeys = Object.keys(mostFavoriteBooksId).filter(key => mostFavoriteBooksId[key] === maxValue);
+
+    books.map((book) => {
+      if(maxKeys.includes((book.id).toString())) mostFavoriteBooksTitle.push(book.titre);
+    });
+
+    return mostFavoriteBooksTitle[0];
+  }
+
   return (
     <div className={'profile'}>
+      {clickOnDelete && <div className={'profile-background-popup-delete'}>
+        <div className={'profile-popup-delete'}>
+          <h2 className={'profile-popup-delete-title'}>Are you sure ?</h2>
+          <div className={'profile-popup-delete-text'}>Are you sure you want to delete your account, once deleted all your data will be lost forever.</div>
+          <div className={'profile-popup-delete-buttons'}>
+            <button onClick={() => cancelDeleteProfile()} className="profile-popup-delete-buttons-cancel">Cancel</button>
+            <button onClick={() => cancelDeleteProfile()} className="profile-popup-delete-buttons-confirm">Confirm</button>
+          </div>
+        </div>
+      </div>}
       <div className={'profile-general-container'}>
         <div className={'profile-general-container-title'}>
           <img src="src\assets\icons\MediumAccountIcon.svg" alt="Account icon" />
@@ -97,7 +210,7 @@ export function ProfilView() {
             <div className={'profile-general-container-content-form-name'}>
               <div className="profile-general-container-content-form-group">
                 <label htmlFor="firstname" className="profile-general-container-content-form-label">
-                  <img src="src\assets\icons\LittleAccountIcon.svg" alt="" />
+                  <img src="src\assets\icons\LittleAccountIcon.svg" alt="Icon account" />
                 </label>
                 <input
                   type="text"
@@ -105,6 +218,9 @@ export function ProfilView() {
                   className="profile-general-container-content-form-input profile-general-container-content-form-input-firstname"
                   placeholder='First Name'
                   required
+                  disabled={!clickOnUpdate}
+                  value={firstname}
+                  onChange={(e) => changeFirstname(e.target.value)}
                 />
               </div>
               <div className="profile-general-container-content-form-group">
@@ -114,12 +230,15 @@ export function ProfilView() {
                   className="profile-general-container-content-form-input profile-general-container-content-form-input-lastname"
                   placeholder='Last Name'
                   required
+                  disabled={!clickOnUpdate}
+                  value={lastname}
+                  onChange={(e) => changeLastname(e.target.value)}
                 />
               </div>
             </div>
             <div className="profile-general-container-content-form-group">
               <label htmlFor="email" className="profile-general-container-content-form-label">
-                <img src="src\assets\icons\LittleEmailIcon.svg" alt="" />
+                <img src="src\assets\icons\LittleEmailIcon.svg" alt="Icon email" />
               </label>
               <input
                 type="email"
@@ -127,11 +246,15 @@ export function ProfilView() {
                 className="profile-general-container-content-form-input"
                 placeholder='Email'
                 required
+                disabled={!clickOnUpdate}
+                autoComplete="username"
+                value={email}
+                onChange={(e) => changeEmail(e.target.value)}
               />
             </div>
-            <div className="profile-general-container-content-form-group">
+            {clickOnUpdate && <div className="profile-general-container-content-form-group">
               <label htmlFor="password" className="profile-general-container-content-form-label">
-                <img src="src\assets\icons\LittlePasswordIcon.svg" alt="" />
+                <img src="src\assets\icons\LittlePasswordIcon.svg" alt="Icon password" />
               </label>
               <input
                 type="password"
@@ -139,11 +262,14 @@ export function ProfilView() {
                 className="profile-general-container-content-form-input"
                 placeholder='Password'
                 required
+                disabled={!clickOnUpdate}
+                autoComplete="new-password"
+                onChange={(e) => changePassword(e.target.value)}
               />
-            </div>
-            <div className="profile-general-container-content-form-group">
+            </div>}
+            {clickOnUpdate && <div className="profile-general-container-content-form-group">
               <label htmlFor="confirm-password" className="profile-general-container-content-form-label">
-                <img src="src\assets\icons\LittlePasswordIcon.svg" alt="" />
+                <img src="src\assets\icons\LittlePasswordIcon.svg" alt="Icon password" />
               </label>
               <input
                 type="password"
@@ -151,74 +277,87 @@ export function ProfilView() {
                 className="profile-general-container-content-form-input"
                 placeholder='Confirm password'
                 required
+                disabled={!clickOnUpdate}
+                autoComplete="new-password"
+                onChange={(e) => changeConfirmPassword(e.target.value)}
               />
-            </div>
-            <div className='profile-general-container-content-form-buttons'>
-              <button type="submit" className="profile-general-container-content-form-buttons-update">
-                Update
-              </button>
-              <button type="submit" className="profile-general-container-content-form-buttons-delete">
-                Delete
-              </button>
-            </div>
+            </div>}
           </form>
+          <div className='profile-general-container-content-form-buttons'>
+            {!clickOnUpdate && <button type="submit" onClick={() => updateProfile()} className="profile-general-container-content-form-buttons-update">
+              Update
+            </button>
+            }
+            {clickOnUpdate && <button type="submit" onClick={() => saveProfile()} className="profile-general-container-content-form-buttons-update">
+              Save
+            </button>
+            }
+            <button type="submit" onClick={() => deleteProfile()} className="profile-general-container-content-form-buttons-delete">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
       <div className={'profile-statistics-container'}>
         <div className={'profile-statistics-container-title'}>
-          <img src="src\assets\icons\MediumStatisticsIcon.svg" alt="Account icon" />
+          <img src="src\assets\icons\MediumStatisticsIcon.svg" alt="Statistic icon" />
           <h2>Statistics</h2>
+        </div>
+        <div className={'profile-statistics-container-content'}>
+          <div className={'profile-statistics-container-content-element'}>
+            <div className={'profile-statistics-container-content-element-image'}>
+              <img src="src\assets\icons\MediumMapIcon.svg" alt="Map icon"/>
+            </div>
+            <div className='profile-statistics-container-content-element-text'>
+              Total adventure: <span className={'profile-statistics-container-content-element-text-data'}>{totalAdventure()}</span>
+            </div>
+          </div>
+          <div className={'profile-statistics-container-content-element'}>
+            <div className={'profile-statistics-container-content-element-image'}>
+              <img src="src\assets\icons\MediumPercentIcon.svg" alt="Percent icon"/>
+            </div>
+            <div className='profile-statistics-container-content-element-text'>
+              Adventure percentage completed: <span className={'profile-statistics-container-content-element-text-data'}>{adventurePercentageCompleted()}%</span>
+            </div>
+          </div>
+          <div className={'profile-statistics-container-content-element'}>
+            <div className={'profile-statistics-container-content-element-image'}>
+              <img src="src\assets\icons\MediumBookIcon.svg" alt="Book icon"/>
+            </div>
+            <div className='profile-statistics-container-content-element-text'>
+              Books finished: <span className={'profile-statistics-container-content-element-text-data'}>{booksFinished()}</span>
+            </div>
+          </div>
+          <div className={'profile-statistics-container-content-element'}>
+            <div className={'profile-statistics-container-content-element-image'}>
+              <img src="src\assets\icons\MediumFavoriteIcon.svg" alt="Favorite icon"/>
+            </div>
+            <div className='profile-statistics-container-content-element-text'>
+              Most favorite books: <span className={'profile-statistics-container-content-element-text-data'}>{mostFavoriteBooks()}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={'profile-favorites-container'}>
+        <div className={'profile-favorites-container-title'}>
+          <img src="src\assets\icons\LittleFavoriteIcon.svg" alt="Favorite icon" />
+          <h2>My favorites</h2>
+        </div>
+        <div className="profile-favorites-container-content">
+          {(favoris.length === 0) && <p>There's nothing here to see for you...</p>}
+          {(favoris.length >= 1) && favoris.map((fav) => fav.livre && (
+            <NavLink to={`/book/${fav.id_livre}`} key={fav.id_livre}>
+              <BookCard
+                book={fav.livre}
+                handleFavourite={() => handleFavourite(fav.id_livre)}
+                books={books}
+                favourites={favoris}
+              />
+            </NavLink>
+
+          ))}
         </div>
       </div>
     </div>
   );
-}
-{
-/*
-<div className="profile-page">
-  <div className="profile-general-container">
-    <div className="profile-info">
-      <img
-        src={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
-        alt="User Avatar" className="profile-avatar" />
-      <div className="profile-details">
-        <h2 className="profile-name">{userData.nom} {userData.prenom}</h2>
-        <span className="profile-email">{userData.mail}</span>
-        <span className="profile-join-date">Member since {userData.creation_date || '29/07/2003'}</span>
-      </div>
-    </div>
-  </div>
-  <div className="profile-favorites">
-    <h2 className="profile-favorites-title">Mes Favoris</h2>
-    <div className="profile-favorites-list">
-      {(favoris.length === 0) && <p>There's nothing here to see for you...</p>}
-      {(favoris.length >= 1) && favoris.map((fav) => fav.livre && (
-        <NavLink to={`/book/${fav.id_livre}`} key={fav.id_livre}>
-          <BookCard
-            book={fav.livre}
-            handleFavourite={() => handleFavourite(fav.id_livre)}
-            books={books}
-            favourites={favoris}
-          />
-        </NavLink>
-
-      ))}
-    </div>
-  </div>
-</div>
-
-
-<div className={'profile-general-container-content-name'}>
-            <span className={'profile-general-container-content-name-firstname'}>{userData.prenom}</span>
-            <span className={'profile-general-container-content-name-lastname'}>{userData.nom}</span>
-          </div>
-          <ul>
-            <li>Email: {userData.mail}</li>
-            <li>Member since: {formattedDate()}</li>
-          </ul>
-          <button
-            type={'button'}>
-            Delete my account
-          </button>
-*/
 }
